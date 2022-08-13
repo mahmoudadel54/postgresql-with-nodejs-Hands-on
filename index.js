01fd5371211f6dc0ece31e4407d35e0236ad2a83
mahmoudadel54 <mahmoodmadel@gmail.com>
2022-08-13 13:45:35 +0200

    exercise 2: add some notes to connection params, implement some queries

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const { Client } = require('pg');
 //2- create an object of client and fill all mandatory params:
 //user[must], pass[must], database name[optional], port[optional], host[optional],
 const client = new Client({
-    user:'postgre',         //enter username of database
+    user:'postgres',         //enter username of database
     password:'admin',       //enter passweord of db
     host:'localhost',       //host name of the machine where the postgres db is installed 
-    port:5432,              //here is the port number
+    port:5432,              //here is the port number: 4532 is the defualt, see this file --> 
+                            //Program Files\PostgreSQL\${VERSION_NUMBER}\data\postgresql.conf
     database:'test',        //database name that you want to connect to
 });
 
@@ -19,6 +20,11 @@ client.connect()
 .then(()=>{
     console.log("Succesfully Connected");
 })
+//we can write query like that: "SELECT * from wcities WHERE city='dubai' OR country='eg'"
+//but the best way to avoid query injection use the below approach
+
+.then(()=>client.query("SELECT * from wcities WHERE city=$1 OR country=$2",['dubai', 'eg'])
+.then(results=>console.table(results.rows)))
 .catch(e=>console.log("error",{e}))
 .finally(()=>client.end())
 
